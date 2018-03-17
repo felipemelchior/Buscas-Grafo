48fb420c0d6d323c4386f3fff8bd24d8ee8f8d12
Felipe <fehmel@gmail.com>
2018-03-17 10:21:00 -0300

    Criacao grafo null

--- grafo.cpp
+++ grafo.cpp
@@ -20,12 +20,22 @@ struct grafo {
 };
 typedef struct 	grafo Grafo;
 
+Grafo* criaGrafo(Grafo* Gr) {
+	if(Gr == NULL) {
+		Gr->m == NULL;		
+	}else{
+		cout << "Grafo ja iniciado" << endl;
+	}
+
+	return Gr;
+}
 
 int main(int argc, char **argv) {
 	ifstream iFile;
 	int nodo, aresta;
 	string Nome;
 
+	Grafo* grafo = NULL;
 
 	if(argc < 2) {
 		cout << "Faltam argumentos!" << endl << "Uso: " << argv[0] << "<ArquivoGrafo.txt>" << endl;
@@ -40,12 +50,22 @@ int main(int argc, char **argv) {
 		exit(1);
 	}
 
-	while(!iFile.eof()) {
+	grafo = criaGrafo(grafo);
+
+	while(!iFile.eof()){
+		iFile >> nodo;
+		iFile >> aresta;
+
+		grafo = criaAresta(int nodo, int aresta);
+
+	}
+
+	/*while(!iFile.eof()) {
 		iFile >> nodo;
 		iFile >> aresta;
 
 		cout << nodo << " --> " << aresta << endl;
-	}
+	}*/
 
 	return 0;
 }
